3b779b6ba7c94117bf0a59a0ca9da5ab17a79903
Adam Hansen <adamlars90@gmail.com>
2018-02-17 10:39:16 +0100

    Added Navbar
    - Implemented Navbar in core module
    - Started styling navbar with links

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatButtonModule, MatToolbarModule} from '@angular/material';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    RouterModule,
+    MatButtonModule
   ],
-  declarations: [PageNotFoundComponent]
+  declarations: [PageNotFoundComponent, NavbarComponent],
+  exports: [
+    NavbarComponent
+  ]
 })
 export class CoreModule { }
