874c78ec4d6cb29597c5bee70d6e1854d1963047
Adam Hansen <adamlars90@gmail.com>
2018-02-20 13:57:24 +0100

    Added login button handling

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -8,12 +8,18 @@ import {FormControl, Validators} from '@angular/forms';
 })
 export class LoginComponent {
 
+  mustEnterValue = 'You must enter a value';
+
   email = new FormControl('', [Validators.required, Validators.email]);
   password = new FormControl('', [Validators.required]);
 
   getErrorMessage() {
-    return this.email.hasError('required') ? 'You must enter a value' :
-      this.email.hasError('email') ? 'Not a valid email' :
+    return this.email.hasError('required') ?
+       this.mustEnterValue :
+      this.email.hasError('email') ?
+        'Not a valid email' :
+        this.password.hasError('required') ?
+          this.mustEnterValue :
         '';
   }
 }
