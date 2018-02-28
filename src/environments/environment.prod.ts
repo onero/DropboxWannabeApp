a1e2ac88b7721626c0cb5c7602b95a5e0c77492a
Adam Hansen <adamlars90@gmail.com>
2018-02-28 17:50:37 +0100

    Fixed environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,11 @@ export const environment = {
   production: true,
   shouldBeMobileFriendly: false,
   firebase: {
-    firebase: {
-      apiKey: 'AIzaSyB-c76-5PoOtPOuHi5e6J1eOp5j4oaNkIc',
-      authDomain: 'dropboxwannabe.firebaseapp.com',
-      databaseURL: 'https://dropboxwannabe.firebaseio.com',
-      projectId: 'dropboxwannabe',
-      storageBucket: 'dropboxwannabe.appspot.com',
-      messagingSenderId: '119642286636'
-    }
+    apiKey: 'AIzaSyB-c76-5PoOtPOuHi5e6J1eOp5j4oaNkIc',
+    authDomain: 'dropboxwannabe.firebaseapp.com',
+    databaseURL: 'https://dropboxwannabe.firebaseio.com',
+    projectId: 'dropboxwannabe',
+    storageBucket: 'dropboxwannabe.appspot.com',
+    messagingSenderId: '119642286636'
   }
 };
