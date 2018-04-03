00633d543bb284e7940b07287f28f176b9a759f2
Adam Hansen <adamlars90@gmail.com>
2018-04-03 18:19:18 +0200

    Implemented onFileClicked

--- src/app/home/file-system/folder/folder.component.ts
+++ src/app/home/file-system/folder/folder.component.ts
@@ -23,18 +23,9 @@ export class FolderComponent implements OnInit {
   ngOnInit() {
   }
 
-  onFileClicked(File: FileModel) {
+  onFileClicked(file: FileModel) {
     // TODO: ALH: Consider file service to get folder
-    const fileToSelect: FileModel = {
-      uid: '2',
-      url: 'https://firebasestorage.googleapis.com/v0/b/dropboxwannabe.appspot.com/o/adamino%2F1520653670895_Profil.jpg?alt=media&token=7f3eea89-00ec-4438-9cf8-2310bba92cba',
-      created: '3/10/2018',
-      displayName: 'Mr. Awesome',
-      fileName: 'mrAwesome.jpg',
-      size: 180,
-      type: 'image/jpeg'
-    };
-    this.fileClicked.emit(fileToSelect);
+    this.fileClicked.emit(file);
   }
 
   onFolderClicked(folder: FolderModel) {
