250fa27da63ee25d84d97c310f31bb0f51e03c55
Adam Hansen <adamlars90@gmail.com>
2018-04-03 10:24:45 +0200

    Added file model

--- src/app/home/file-system/folder/folder.component.ts
+++ src/app/home/file-system/folder/folder.component.ts
@@ -16,6 +16,17 @@ export class FolderComponent implements OnInit {
     this.currentFolder = {
       displayName: 'root',
       uid: '1',
+      files: [
+        {
+          uid: '1',
+          url: 'https://firebasestorage.googleapis.com/v0/b/dropboxwannabe.appspot.com/o/adamino%2F1520711029667_the_hobbit_the_battle_of_the_five_armies-wide.jpg?alt=media&token=bc01f8d6-ac9f-4a6a-9a4c-15b21a28a601',
+          created: '3/10/2018',
+          displayName: 'Awesome',
+          fileName: 'awesome.jpg',
+          size: 3450,
+          type: 'image/jpeg'
+        }
+      ],
       subFolders: [
         {
           uid: '2',
--- src/app/home/file-system/shared/folder.model.ts
+++ src/app/home/file-system/shared/folder.model.ts
@@ -1,5 +1,8 @@
+import {FileModel} from './file.model';
+
 export interface FolderModel {
   uid: string;
   displayName: string;
+  files?: FileModel[];
   subFolders?: FolderModel[];
 }
