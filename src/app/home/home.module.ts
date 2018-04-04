9eeaf5bb0412b19347fc29365a47f31de9a1954f
Adam Hansen <adamlars90@gmail.com>
2018-04-04 07:37:26 +0200

    Updated module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,21 +3,19 @@ import {CommonModule} from '@angular/common';
 import {FileSystemComponent} from './file-system/file-system.component';
 import {CoreModule} from '../core/core.module';
 import {StorageService} from './file-system/shared/storage.service';
-import {MatButtonModule, MatCardModule, MatGridListModule, MatIconModule} from '@angular/material';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatCardModule, MatGridListModule} from '@angular/material';
 import {AngularFireStorageModule} from 'angularfire2/storage';
-import {DropZoneDirective} from '../shared/directives/drop-zone.directive';
-import { FileSizePipe } from './file-system/shared/file-size.pipe';
+import {FileSizePipe} from './file-system/shared/file-size.pipe';
 import {AngularFirestoreModule} from 'angularfire2/firestore';
 import {NgxGalleryModule} from 'ngx-gallery';
-import { ImageGalleryComponent } from './file-system/image-gallery/image-gallery.component';
-import { UploadZoneComponent } from './file-system/upload-zone/upload-zone.component';
-import {Ng4LoadingSpinnerModule} from 'ng4-loading-spinner';
-import {UnifiedMaterialModule} from '../shared/styling/unified-material.module';
+import {ImageGalleryComponent} from './file-system/image-gallery/image-gallery.component';
+import {UploadZoneComponent} from './file-system/upload-zone/upload-zone.component';
 import {SharedModule} from '../shared/shared.module';
-import { FileService } from './file-system/shared/file.service';
-import { FolderService } from './file-system/shared/folder.service';
-import { FolderComponent } from './file-system/folder/folder.component';
+import {FileService} from './file-system/shared/file.service';
+import {FolderService} from './file-system/shared/folder.service';
+import {FolderComponent} from './file-system/folder/folder.component';
+import {ContainerColumnComponent} from './file-system/container-column/container-column.component';
+import {FileColumnComponent} from './file-system/file/file-column.component';
 
 @NgModule({
   imports: [
@@ -30,7 +28,15 @@ import { FolderComponent } from './file-system/folder/folder.component';
     MatGridListModule,
     NgxGalleryModule
   ],
-  declarations: [FileSystemComponent, FileSizePipe, ImageGalleryComponent, UploadZoneComponent, FolderComponent],
+  declarations: [
+    FileSystemComponent,
+    FileSizePipe,
+    ImageGalleryComponent,
+    UploadZoneComponent,
+    FolderComponent,
+    ContainerColumnComponent,
+    FileColumnComponent],
   providers: [StorageService, FileService, FolderService]
 })
-export class HomeModule { }
+export class HomeModule {
+}
